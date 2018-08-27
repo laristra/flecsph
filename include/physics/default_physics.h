78dccb97d51f1f838fb9a237fcf03cc84a7a21d8
Hyun Lim <hylim1988@gmail.com>
2018-08-27 08:10:25 -0600

    More kernel options into param

--- include/physics/default_physics.h
+++ include/physics/default_physics.h
@@ -82,20 +82,22 @@ namespace physics{
       kernel = kernels::wendland_quintic;
       gradKernel = kernels::gradient_wendland_quintic;
     }
-    #if 0
+    else if (boost::iequals(kstr, "super gaussian")) {
+      kernel = kernels::super_gaussian;
+      gradKernel = kernels::gradient_super_gaussian;
+    }
     else if (boost::iequals(kstr, "wqc4")) {
       kernel = kernels::wqc4;
       gradKernel = kernels::gradient_wqc4;
     }
     else if (boost::iequals(kstr, "wqc6")) {
-      kernel = kernels::wqc4;
+      kernel = kernels::wqc6;
       gradKernel = kernels::gradient_wqc6;
     }
     else if (boost::iequals(kstr, "sinc")) {
-      kernel = kernels::sinc;
-      gradKernel = kernels::gradient_sinc;
+      kernel = kernels::sinc_ker;
+      gradKernel = kernels::gradient_sinc_ker;
     }
-    #endif
     else {
       clog_one(fatal) << "Bad kernel parameter" << std::endl;
     }
