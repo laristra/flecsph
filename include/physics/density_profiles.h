965d487e8168573ff688b9671dd5e80d063b3e6d
Oleg Korobkin <o.korobkin@gmail.com>
2019-01-26 14:41:16 -0700

    (+) 'mesa' density profile

--- include/physics/density_profiles.h
+++ include/physics/density_profiles.h
@@ -46,6 +46,10 @@ namespace density_profiles {
   static radial_function_t spherical_mass_profile = NULL;
   static radial_function_t spherical_drho_dr = NULL;
 
+  // an important constant for the mesa density
+  static double mesa_rho0;
+  const  double mesa_q = 0.25; // ratio of the slope width to the radius
+
   /**
    * @brief  constant uniform density in a domain of radius R = 1
    * @param  r     - spherical radius
@@ -79,6 +83,45 @@ namespace density_profiles {
   }
 
   /**
+   * @brief  "mesa" density: flat top and steep slopes
+   * @param  r     - spherical radius
+   */
+  double mesa_mass_helper(const double r) { 
+    const double dr = mesa_q, r0 = 1. - mesa_q;
+    return CU(r0)/3.
+       + (CU(r)-CU(r0))/3.*(1. - SQ(r0)/SQ(dr))
+       + r0*(SQ(r)-SQ(r0))*(SQ(r)+SQ(r0))/(2.*SQ(dr))
+       - (SQ(r)*CU(r)-SQ(r0)*CU(r0))/(5*SQ(dr)); 
+  }
+
+  double rho_mesa_density(const double r) {
+    double rho = 0.0;
+    const double r0 = 1. - mesa_q;
+    if (r < 1.-mesa_q) 
+      rho = mesa_rho0;
+    else if (r < 1.)
+      rho = mesa_rho0*(1. - SQ(r-r0)/SQ(mesa_q));
+    return rho;
+  }
+
+  double mass_mesa_density(const double r) {
+    double m = 0.0;
+    if (r < 1.-mesa_q) 
+      m = 4.*M_PI/3. * mesa_rho0 * CU(r);
+    else if (r - 1. < 1e-12)
+      m = 4.*M_PI*mesa_rho0*mesa_mass_helper(r);
+    return m;
+  }
+
+  double drhodr_mesa_density(const double r) {
+    double drhodr = 0.0;
+    const double r0 = 1. - mesa_q;
+    if (r > 1.-mesa_q and r < 1.) 
+      drhodr = -2.*mesa_rho0*(r-r0)/SQ(mesa_q);
+    return drhodr;
+  }
+
+  /**
    * @brief      External force selector
    * @param      efstr    ext. force string
    */
@@ -93,6 +136,12 @@ namespace density_profiles {
       spherical_density_profile = rho_parabolic_density;
       spherical_mass_profile = mass_parabolic_density;
       spherical_drho_dr = drhodr_parabolic_density;
+    }
+    else if (boost::iequals(density_profile,"mesa")) {
+      spherical_density_profile = rho_mesa_density;
+      spherical_mass_profile = mass_mesa_density;
+      spherical_drho_dr = drhodr_mesa_density;
+      mesa_rho0 = 1./(4.*M_PI*mesa_mass_helper(1.));
     }
     else {
       clog(error) << "ERROR: wrong parameter in density_profiles";
